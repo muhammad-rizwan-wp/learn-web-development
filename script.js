760267bc56599e91e37c2125daedb3107fc53bac
Muhammad Rizwan <muhammad.rizwan1998@yahoo.com>
2026-03-27 16:57:02 +0500

    Day 3 Dose

--- script.js
+++ script.js
@@ -11,11 +11,11 @@ document.title = "My Page";
 document.body.style.backgroundColor = "lightblue";
 
 /**
-* What is a FUNCTION?
-* A function is a reusable block of code that performs a specific task.
-* Functions are like action words (verbs).
-* Inside the parentheses, we define PARAMETERS (placeholders for data).
-* When we call the function, we pass ARGUMENTS (actual values).
+ * What is a FUNCTION?
+ * A function is a reusable block of code that performs a specific task.
+ * Functions are like action words (verbs).
+ * Inside the parentheses, we define PARAMETERS (placeholders for data).
+ * When we call the function, we pass ARGUMENTS (actual values).
  */
 
 /* CREATION */
@@ -34,4 +34,35 @@ function sqare(num) {
 // greet();
 greeting("Muhammad Rizwan", "Asslamualikum");
 alert(sqare(10));
+/*----------------------------------------------- END -----------------------------------------------------*/
+
+/*---------------------------------------------- START ----------------------------------------------------*/
+/* ===> Day 3 <=== */
+/**
+ * Object
+ * Inside object the function of keyword we doesn't mentioned
+ * We can add a object inside object (NEST-OBJECT)
+ * */
+
+let catName = "Neeon";
+let catAge = 5;
+
+function meow() {
+  alert("Meooooowwwwwww");
+}
+
+let cat = {
+  name: "Neeon",
+  age: 5,
+  eyeColor: "Blue",
+  meow() {
+    alert("Meoooowwwwww");
+  },
+  food: {
+    favorite: "Tuna",
+    leastFavorite: "Banana",
+  },
+};
+cat.meow();
+cat.food.favorite;
 /*----------------------------------------------- END -----------------------------------------------------*/
