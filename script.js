a0454486e2d48efe5e01280f54265349cdce1c3f
Muhammad Rizwan <muhammad.rizwan1998@yahoo.com>
2026-03-27 11:38:50 +0500

    Day 2 Dose

--- script.js
+++ script.js
@@ -1,15 +1,37 @@
-
-
-/*---------------------------------------------- START ----------------------------------------------------*/ 
+/*---------------------------------------------- START ----------------------------------------------------*/
 
 /* ===> Day 1 & 2 <=== */
 /**
  * JS is bigger than the WEB-BROWSER
  * In JavaScript, anything wrapped in quotes is treated as a string.
  *
-* */ 
+ * */
 
-document.title = 'My Page';
+document.title = "My Page";
 document.body.style.backgroundColor = "lightblue";
 
-/*----------------------------------------------- END -----------------------------------------------------*/ 
+/**
+ * What is FUNCTION?
+ * Functions are as action words
+ * It would be a verb
+ * Inside the parenthesis we can pass the data to perform the action
+ * The data techinincal name is arguments
+ */
+
+/* CREATION */
+function greet() {
+  alert("Hi, Dev 😃");
+}
+
+function greeting(name, message) {
+  alert(name + ", " + message + ".");
+}
+
+function sqare(num) {
+  return num * num;
+}
+/* INVOKATION */
+// greet();
+greeting("Muhammad Rizwan", "Asslamualikum");
+alert(sqare(10));
+/*----------------------------------------------- END -----------------------------------------------------*/
