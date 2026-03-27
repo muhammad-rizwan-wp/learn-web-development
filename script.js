a45e2ffeb9b9a04827f6a0ce14a0c8e1366495b2
Muhammad Rizwan <muhammad.rizwan1998@yahoo.com>
2026-03-27 10:26:47 +0500

    Day 1 Dose

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
 
+
+/*---------------------------------------------- START ----------------------------------------------------*/ 
+
+/* ===> Day 1 & 2 <=== */
+/**
+ * JS is bigger than the WEB-BROWSER
+ * In JavaScript, anything wrapped in quotes is treated as a string.
+ *
+* */ 
+
+document.title = 'My Page';
+document.body.style.backgroundColor = "lightblue";
+
+/*----------------------------------------------- END -----------------------------------------------------*/ 
