e4c913fe2a826e851ab64686ed39d8150581c5de
Muhammad Rizwan <muhammad.rizwan1998@yahoo.com>
2026-04-10 10:52:27 +0500

    Color Changer Final Solution Code

--- tasks/colorchanger/app.js
+++ tasks/colorchanger/app.js
@@ -1,1 +1,23 @@
+const body = document.querySelector("body");
+const buttons = document.querySelectorAll(".button");
 
+buttons.forEach((button) => {
+  button.addEventListener("click", (e) => {
+    switch (e.target.id) {
+      case "grey":
+        body.style.backgroundColor = `${e.target.id}`;
+        break;
+      case "white":
+        body.style.backgroundColor = `${e.target.id}`;
+        break;
+      case "blue":
+        body.style.backgroundColor = `${e.target.id}`;
+        break;
+      case "yellow":
+        body.style.backgroundColor = `${e.target.id}`;
+        break;
+      default:
+        body.style.backgroundColor = `${e.target.id}`;
+    }
+  });
+});
